63720e87a566de2dddc2e738e6a4bad0773c51fa
Yen Tran <yen.tran@beaconinteractive.com>
2013-10-10 23:28:51 -0400

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace A1Calculator
+{
+   partial class Main
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.menuStrip = new System.Windows.Forms.MenuStrip();
+         this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+         this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+         this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+         this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+         this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.txtOutput = new System.Windows.Forms.TextBox();
+         this.txtInput = new System.Windows.Forms.TextBox();
+         this.groupBox1 = new System.Windows.Forms.GroupBox();
+         this.btnConvert = new System.Windows.Forms.Button();
+         this.groupBox2 = new System.Windows.Forms.GroupBox();
+         this.txtListing = new System.Windows.Forms.TextBox();
+         this.menuStrip.SuspendLayout();
+         this.groupBox1.SuspendLayout();
+         this.groupBox2.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // menuStrip
+         // 
+         this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.helpToolStripMenuItem1});
+         this.menuStrip.Location = new System.Drawing.Point(0, 0);
+         this.menuStrip.Name = "menuStrip";
+         this.menuStrip.Size = new System.Drawing.Size(358, 24);
+         this.menuStrip.TabIndex = 0;
+         this.menuStrip.Text = "menuStrip\\";
+         // 
+         // fileToolStripMenuItem1
+         // 
+         this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem1});
+         this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+         this.fileToolStripMenuItem1.Size = new System.Drawing.Size(40, 20);
+         this.fileToolStripMenuItem1.Text = "&File";
+         // 
+         // exitToolStripMenuItem1
+         // 
+         this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+         this.exitToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+         this.exitToolStripMenuItem1.Size = new System.Drawing.Size(154, 22);
+         this.exitToolStripMenuItem1.Text = "E&xit";
+         this.exitToolStripMenuItem1.Click += new System.EventHandler(this.OnExit);
+         // 
+         // helpToolStripMenuItem1
+         // 
+         this.helpToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+         this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+         this.helpToolStripMenuItem1.Size = new System.Drawing.Size(45, 20);
+         this.helpToolStripMenuItem1.Text = "&Help";
+         // 
+         // aboutToolStripMenuItem1
+         // 
+         this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+         this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(122, 22);
+         this.aboutToolStripMenuItem1.Text = "&About";
+         this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.OnMenuHelpAbout);
+         // 
+         // fileToolStripMenuItem
+         // 
+         this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+         this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+         this.fileToolStripMenuItem.Text = "&File";
+         // 
+         // exitToolStripMenuItem
+         // 
+         this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+         this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+         this.exitToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+         this.exitToolStripMenuItem.Text = "E&xit";
+         this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnExit);
+         // 
+         // helpToolStripMenuItem
+         // 
+         this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+         this.helpToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+         this.helpToolStripMenuItem.Text = "&Help";
+         // 
+         // aboutToolStripMenuItem
+         // 
+         this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+         this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.aboutToolStripMenuItem.Text = "&About";
+         this.aboutToolStripMenuItem.Click += new System.EventHandler(this.OnMenuHelpAbout);
+         // 
+         // txtOutput
+         // 
+         this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtOutput.Location = new System.Drawing.Point(218, 19);
+         this.txtOutput.Name = "txtOutput";
+         this.txtOutput.ReadOnly = true;
+         this.txtOutput.Size = new System.Drawing.Size(100, 30);
+         this.txtOutput.TabIndex = 3;
+         // 
+         // txtInput
+         // 
+         this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtInput.Location = new System.Drawing.Point(15, 19);
+         this.txtInput.Name = "txtInput";
+         this.txtInput.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+         this.txtInput.Size = new System.Drawing.Size(98, 30);
+         this.txtInput.TabIndex = 1;
+         this.txtInput.TextChanged += new System.EventHandler(this.OnInputTextChanged);
+         // 
+         // groupBox1
+         // 
+         this.groupBox1.Controls.Add(this.btnConvert);
+         this.groupBox1.Controls.Add(this.txtInput);
+         this.groupBox1.Controls.Add(this.txtOutput);
+         this.groupBox1.Location = new System.Drawing.Point(13, 27);
+         this.groupBox1.Name = "groupBox1";
+         this.groupBox1.Size = new System.Drawing.Size(333, 57);
+         this.groupBox1.TabIndex = 3;
+         this.groupBox1.TabStop = false;
+         // 
+         // btnConvert
+         // 
+         this.btnConvert.Enabled = false;
+         this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.84615F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.btnConvert.Image = global::A1Calculator.Properties.Resources.b;
+         this.btnConvert.Location = new System.Drawing.Point(119, 19);
+         this.btnConvert.Name = "btnConvert";
+         this.btnConvert.Size = new System.Drawing.Size(93, 30);
+         this.btnConvert.TabIndex = 2;
+         this.btnConvert.UseVisualStyleBackColor = true;
+         this.btnConvert.Click += new System.EventHandler(this.OnConvert);
+         // 
+         // groupBox2
+         // 
+         this.groupBox2.Controls.Add(this.txtListing);
+         this.groupBox2.Location = new System.Drawing.Point(13, 90);
+         this.groupBox2.Name = "groupBox2";
+         this.groupBox2.Size = new System.Drawing.Size(333, 180);
+         this.groupBox2.TabIndex = 4;
+         this.groupBox2.TabStop = false;
+         // 
+         // txtListing
+         // 
+         this.txtListing.Font = new System.Drawing.Font("Courier New", 8.307693F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtListing.Location = new System.Drawing.Point(15, 19);
+         this.txtListing.Multiline = true;
+         this.txtListing.Name = "txtListing";
+         this.txtListing.ReadOnly = true;
+         this.txtListing.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+         this.txtListing.Size = new System.Drawing.Size(303, 145);
+         this.txtListing.TabIndex = 4;
+         // 
+         // Main
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(358, 282);
+         this.Controls.Add(this.groupBox2);
+         this.Controls.Add(this.groupBox1);
+         this.Controls.Add(this.menuStrip);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.MainMenuStrip = this.menuStrip;
+         this.MaximizeBox = false;
+         this.Name = "Main";
+         this.Text = "A1 Calculator";
+         this.menuStrip.ResumeLayout(false);
+         this.menuStrip.PerformLayout();
+         this.groupBox1.ResumeLayout(false);
+         this.groupBox1.PerformLayout();
+         this.groupBox2.ResumeLayout(false);
+         this.groupBox2.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.MenuStrip menuStrip;
+      private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+      private System.Windows.Forms.TextBox txtOutput;
+      private System.Windows.Forms.TextBox txtInput;
+      private System.Windows.Forms.GroupBox groupBox1;
+      private System.Windows.Forms.GroupBox groupBox2;
+      private System.Windows.Forms.TextBox txtListing;
+      private System.Windows.Forms.Button btnConvert;
+      private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+      private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+      private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+      private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+   }
+}
+
---
+++ Main.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace A1Calculator
+{
+   public partial class Main : Form
+   {
+      #region Fields
+
+      private const int MAX_COLUMNS = 256;
+      private const string INVALID_OUTPUT_MESSAGE = "NA";
+      private const int NUM_COLUMNS = 5;
+
+      private Color GOOD_INPUT_COLOR = Color.FromArgb(198, 239, 206);
+      private Color BAD_INPUT_COLOR = Color.FromArgb(255, 199, 206);
+
+      private Regex regA1Format = new Regex(@"^[a-zA-Z][a-zA-Z]?$");
+
+      private string _output = string.Empty;
+
+      #endregion
+
+      public Main()
+      {
+         InitializeComponent();
+      }
+
+      protected override void OnLoad(EventArgs e)
+      {
+         base.OnLoad(e);
+
+         txtListing.Text = GenerateListing();
+      }
+
+      private void OnMenuHelpAbout(object sender, EventArgs e)
+      {
+         var aboutBox = new AboutBox();
+         aboutBox.ShowDialog();
+      }
+
+      private void OnExit(object sender, EventArgs e)
+      {
+         Application.Exit();
+      }
+
+      private void OnConvert(object sender, EventArgs e)
+      {
+         if (_output != string.Empty)
+            txtOutput.Text = _output;
+         else
+            txtOutput.Text = INVALID_OUTPUT_MESSAGE;
+      }
+
+      private void OnInputTextChanged(object sender, EventArgs e)
+      {
+         string input = txtInput.Text.Trim();
+         int index;
+         string a1;
+
+         if (input.Length == 0)
+         {
+            _output = string.Empty;
+            txtOutput.Text = string.Empty;
+            btnConvert.Enabled = false;
+            txtInput.BackColor = Color.White;
+         }
+         else if (A1ToIndex(input, out index))
+         {
+            _output = index.ToString();
+            txtOutput.Text = string.Empty;
+            btnConvert.Enabled = true;
+            txtInput.BackColor = GOOD_INPUT_COLOR;
+         }
+         else if (IndexToA1(input, out a1))
+         {
+            _output = a1;
+            txtOutput.Text = string.Empty;
+            btnConvert.Enabled = true;
+            txtInput.BackColor = GOOD_INPUT_COLOR;
+         }
+         else
+         {
+            _output = string.Empty;
+            txtOutput.Text = INVALID_OUTPUT_MESSAGE;
+            btnConvert.Enabled = false;
+            txtInput.BackColor = BAD_INPUT_COLOR;
+         }
+      }
+
+      private bool A1ToIndex(string input, out int output)
+      {
+         output = int.MaxValue;
+
+         if (!regA1Format.IsMatch(input))
+            return false;
+
+         if (input.Length == 1)
+         {
+            char c = input.ToUpper()[0];
+            output = c - 'A';
+            return true;
+         }
+         else
+         {
+            char c1 = input.ToUpper()[0];
+            char c2 = input.ToUpper()[1];
+            int x = c1 - 'A' + 1;
+            int y = c2 - 'A';
+            int index = x * 26 + y;
+            if (index < MAX_COLUMNS)
+            {
+               output = index;
+               return true;
+            }
+           return false;
+         }
+      }
+
+      private bool IndexToA1(string input, out string output)
+      {
+         output = string.Empty;
+
+         int index;
+         if (!int.TryParse(input, out index) || index >= MAX_COLUMNS)
+            return false;
+
+         int x = index / 26;
+         int y = index % 26;
+         if (x == 0)
+            output = Convert.ToString((char)('A' + y));
+         else
+         {
+            char c1 = (char)('A' + x - 1);
+            char c2 = (char)('A' + y);
+            output = string.Format("{0}{1}", c1, c2);
+         }
+         return true;
+      }
+
+      private string GenerateListing()
+      {
+         var builder = new StringBuilder();
+         int currIndex;
+         int numRows = (int)Math.Ceiling((double)MAX_COLUMNS / NUM_COLUMNS);
+         for (int i = 0; i < (numRows - 1) * NUM_COLUMNS; i += NUM_COLUMNS)
+         {
+            for (int j = 0; j < NUM_COLUMNS; ++j)
+            {
+               currIndex = i + j;
+               PrintEntry(builder, currIndex);
+            }
+            builder.AppendLine();
+         }
+         // Make sure we print the right number of columns in the last row.
+         // It's simpler to simply check every row to make sure the index 
+         // doesn't exceed MAX_COLUMNS, but not as efficient.
+         for (int j = 0; j < NUM_COLUMNS; ++j)
+         {
+            currIndex = j + (numRows - 1) * NUM_COLUMNS;
+            if (currIndex < MAX_COLUMNS)
+               PrintEntry(builder, currIndex);
+         }
+
+         return builder.ToString();
+      }
+
+      private void PrintEntry(StringBuilder builder, int index)
+      {
+         string output;
+         builder.AppendFormat("{0,3} ", index);
+         IndexToA1(index.ToString(), out output);
+         builder.AppendFormat("{0,2}", output);
+         builder.Append("\t");
+      }
+
+   }  // class
+}  // ns
